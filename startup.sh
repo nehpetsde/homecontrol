1ded6d14c941b8f42533b1f1627ad3c71b6dea0b
Stephen Tiedemann <stephen.tiedemann@gmail.com>
2019-07-23 19:37:04 +0200

    Newer tmux doesn't like the set-raim-on-exit, just use read.

--- startup.sh
+++ startup.sh
@@ -11,13 +11,10 @@ if tmux list-sessions | grep -E '^homecontrol:'; then
 	tmux kill-session -t homecontrol
 fi
 
-tmux new-session -d -s homecontrol -n shell '/bin/bash'
-tmux set-option -t homecontrol set-remain-on-exit on
-
-tmux new-window -t homecontrol -n fritzlog    "python ~/homecontrol/fritzlog.py"
-tmux new-window -t homecontrol -n gardenlight "python ~/homecontrol/gardenlight.py"
-tmux new-window -t homecontrol -n colorlight  "python ~/homecontrol/colorlight.py"
-tmux new-window -t homecontrol -n gardenwater "python ~/homecontrol/watering.py"
-tmux new-window -t homecontrol -n thermostat  "python ~/homecontrol/thermostat.py"
-
-#tmux new-window -t homecontrol -n starlight "python ~/homecontrol/starlight.py"
+tmux new-session -d -s homecontrol -n shell   "/bin/bash"
+tmux new-window -t homecontrol -n fritzlog    "python ~/homecontrol/fritzlog.py; read"
+tmux new-window -t homecontrol -n gardenlight "python ~/homecontrol/gardenlight.py; read"
+tmux new-window -t homecontrol -n colorlight  "python ~/homecontrol/colorlight.py; read"
+tmux new-window -t homecontrol -n gardenwater "python ~/homecontrol/watering.py; read"
+tmux new-window -t homecontrol -n thermostat  "python ~/homecontrol/thermostat.py; read"
+#tmux new-window -t homecontrol -n starlight   "python ~/homecontrol/starlight.py; read"
